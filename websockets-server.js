9aa93dc40fe0277e68cd10132c2d3cb779986dfa
Eric <eric.edelman24@gmail.com>
2020-04-28 23:07:29 -0700

    updating with front end

--- websockets-server.js
+++ websockets-server.js
@@ -5,29 +5,21 @@ var ws = new WebSocketServer({
     port: port
 });
 var messages = [];
-var topic = "";
 
 console.log('websockets server started');
 
-ws.on('connection', function (socket) {
+ws.on('connection', function (socket){
     console.log('client connection established');
 
-    messages.forEach(function (msg) {
+    messages.forEach(function(msg) {
         socket.send(msg);
-    });
+    })
 
-    socket.send("*** Topic is \'" + topic + "\' ***");
-    socket.on('message', function (data) {
+    socket.on('message', function(data){
         console.log('message recieved: ' + data);
-        if(data.substring(0, 6) === '/topic') {
-            topic = data.substring(7);
-            newData = "*** Topic has changed to \'" + topic + "\' ***"
-        } else {
-            messages.push(data);
-            newData = data;
-        }
+        messages.push(data);
         ws.clients.forEach(function (clientSocket) {
-            clientSocket.send(newData);
+            clientSocket.send(data);
         });
     });
 });
